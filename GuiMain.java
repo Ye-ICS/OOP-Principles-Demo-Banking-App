a2e48fa24c4606516916458ed0f1730b135b905b
MrYe070 <102677547+MrYe070@users.noreply.github.com>
2025-04-30 10:48:55 -0400

    Main menu layout
    Main menu layout fxml
    GUI application entry point

---
+++ GuiMain.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GuiMain extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Banking App 3000");
+        stage.show();
+    }
+}
